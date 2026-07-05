b38c2e2a5220ba3f595345a0ed2aa0d7382a9d46
Hosein <hgh.elnino@google.com>
2026-07-05 15:49:06 +0330

    Move LCD DC/RST to GPIO20 and GPIO21, free all ADC pins

--- main/main.c
+++ main/main.c
@@ -21,8 +21,8 @@
 #define LCD_SCLK        6
 #define LCD_MOSI        7
 #define LCD_CS          10
-#define LCD_DC          4
-#define LCD_RST         3
+#define LCD_DC          20
+#define LCD_RST         21
 #define LCD_W           240
 #define LCD_H           240
 #define LCD_SPI_MHZ     27
