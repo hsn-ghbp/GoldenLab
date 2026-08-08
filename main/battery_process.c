ad003e5d187ac5247ac9f8b9ec9e0e3b4a099377
Hosein <hgh.elnino@google.com>
2026-08-08 16:39:43 +0330

    ADC for battery added

--- main/battery_process.c
+++ main/battery_process.c
@@ -1,6 +1,8 @@
 #include "battery_process.h"
 #include "esp_log.h"
 
+#include "driver/adc.h"
+
 static const char *TAG = "battery_process";
 
 #define BATTERY_MIN_MV      3300
@@ -11,10 +13,23 @@ static uint16_t s_battery_voltage_mv = 0;
 static uint8_t s_battery_percent = 0;
 static battery_level_t s_battery_level = BATTERY_LEVEL_FULL;
 
+
+static uint16_t battery_adc_read_raw(void)
+{
+    int raw = adc1_get_raw(ADC1_CHANNEL_0);
+
+    if (raw < 0) {
+        raw = 0;
+    }
+
+    return (uint16_t)raw;
+}
+
 static uint16_t battery_process_read_adc(void)
 {
     // TODO: replace with real ADC read
-    return 2047 + (rand() % (2606 - 2047 + 1));
+    //return 2047 + (rand() % (2606 - 2047 + 1));
+    return battery_adc_read_raw();
 }
 
 static uint16_t battery_process_adc_to_mv(uint16_t adc_value)
@@ -50,7 +65,6 @@ static battery_level_t battery_process_percent_to_level(uint8_t percent)
     } else if (percent <= 85) {
         return BATTERY_LEVEL_75;
     }
-
     return BATTERY_LEVEL_FULL;
 }
 
@@ -60,6 +74,8 @@ void battery_process_init(void)
     s_battery_voltage_mv = 0;
     s_battery_percent = 0;
     s_battery_level = BATTERY_LEVEL_EMPTY;
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
 
     ESP_LOGI(TAG, "Battery process initialized");
 }
